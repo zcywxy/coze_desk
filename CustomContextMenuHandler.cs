1a824f07069e81bc74f5a6520eeee13d2ec90659
sn78039 <zhoucy@scc.com.cn>
2024-02-27 09:34:40 +0800

    init

---
+++ CustomContextMenuHandler.cs
@@ -0,0 +1,84 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coze
+{
+    public class CustomContextMenuHandler : IContextMenuHandler
+    {
+        private const int RefreshPageCommandId = 26502; // 自定义命令，使用唯一的ID
+        private const int RefreshLayOutCommandId = 26503; // 自定义命令，使用唯一的ID
+        private const int CopyCommandId = 26504; // 自定义命令，使用唯一的ID
+
+        public void OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+        {
+            // 清除默认菜单
+            model.Clear();
+
+            // 添加刷新页面菜单项
+            model.AddItem((CefMenuCommand)RefreshPageCommandId, "刷新页面");
+            model.AddItem((CefMenuCommand)RefreshLayOutCommandId, "修改布局");
+            // 如果可以复制文本，则添加复制菜单项
+            if (!string.IsNullOrWhiteSpace(parameters.SelectionText))
+            {
+                model.AddItem((CefMenuCommand)CopyCommandId, "复制");
+            }
+
+            // 其他菜单项...
+        }
+
+        public bool OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+        {
+            switch ((int)commandId)
+            {
+                case RefreshPageCommandId:
+                    // 执行刷新页面操作
+                    browser.Reload();
+                    return true;
+                case RefreshLayOutCommandId:
+                    RefreshLayOut(browser);
+                    return true;
+                case CopyCommandId:
+                    Copy(frame);
+                    return true;
+            }
+            return false;
+        }
+
+
+        public void Copy(IFrame frame)
+        {
+            frame.Copy();
+        }
+
+
+        private static void RefreshLayOut(IBrowser browser)
+        {
+            string script = @"setInterval(function() {
+    let element = document.querySelector('.sidesheet-container');
+    if(element) {
+		element.style.gridTemplateColumns='0fr 6fr 20fr';
+        element.style.marginLeft = '200px'; // 设置左边距为20像素
+        element.style.marginRight = '200px'; // 设置右边距为20像素
+        clearInterval(element);  // 元素找到后清除定期检查
+    }
+    }, 1000);  // 每1000毫秒（即1秒）检查一次";
+            browser.EvaluateScriptAsync(script);
+        }
+
+        public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+        {
+            // 当上下文菜单被关闭时触发
+        }
+
+        public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+        {
+            // 返回false以显示上下文菜单
+            return false;
+        }
+    }
+
+}
---
+++ MainForm.cs
@@ -0,0 +1,44 @@
+﻿using CefSharp.WinForms;
+using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Coze
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            var cache = Path.GetFullPath("cefcache");
+            if (!System.IO.Directory.Exists(cache))
+            {
+                System.IO.Directory.CreateDirectory(cache);
+            }
+
+            var settings = new CefSettings();
+            settings.CachePath = cache;
+            settings.IgnoreCertificateErrors = true;
+            settings.PersistSessionCookies = true;
+            settings.Locale = "zh-CN";
+            settings.CefCommandLineArgs.Add("--ignore-urlfetcher-cert-requests", "1");
+            settings.CefCommandLineArgs.Add("--ignore-certificate-errors", "1");
+            settings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0";
+            Cef.Initialize(settings);
+            InitializeComponent();
+            chromeBrowser.MenuHandler = new CustomContextMenuHandler();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chromeBrowser.LoadUrlAsync("https://www.coze.com/space/");
+        }
+    }
+}
